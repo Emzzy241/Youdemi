e9b2cf7bc89a64d55470b97fb2fbd33efff583b9
Emzzy241 <emzzyoluwole@gmail.com>
2025-07-02 00:17:22 +0100

    update authentication middleware to extrat token before verifying

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -1,15 +1,48 @@
-import jwt, { decode } from 'jsonwebtoken'
+import jwt from 'jsonwebtoken';
 
 function authMiddleware(req, res, next) {
-    const token = req.headers['authorization']
+  const authHeader = req.headers.authorization; 
+  console.log('Incoming Auth header:', authHeader);
 
-    if (!token) { return res.status(401).json({ message: 'No token provided' })}
+  if (!authHeader) {
+    return res.status(401).json({ message: 'No token provided' });
+  }
 
-    jwt.verify(token, process.env.JWT_SECRET_KEY, (err, decoded) => {
-        if (err) { return res.status(401).json({ message: 'Invalid token' })}
-        req.userId = decoded.id
-        next()
-    })
+// extracting the token before verifying
+  const [scheme, token] = authHeader.split(' ');
+  if (scheme !== 'Bearer' || !token) {
+    return res.status(401).json({ message: 'Invalid authorization format' });
+  }
+
+  jwt.verify(token, process.env.JWT_SECRET_KEY, (err, decoded) => {
+    if (err) {
+      console.log('JWT verify failed:', err.message);
+      return res.status(401).json({ message: 'Invalid token' });
+    }
+
+    console.log('JWT decoded payload:', decoded);
+    req.userId = decoded.id;
+    next();
+  });
 }
 
-export default authMiddleware
+export default authMiddleware;
+
+
+
+// import jwt, { decode } from 'jsonwebtoken'
+
+// function authMiddleware(req, res, next) {
+//     console.log('Incoming Auth header:', req.headers.authorization);
+//     const token = req.headers['authorization']
+
+//     if (!token) { return res.status(401).json({ message: 'No token provided' })}
+
+//     jwt.verify(token, process.env.JWT_SECRET_KEY, (err, decoded) => {
+//         if (err) { return res.status(401).json({ message: 'Invalid token' })}
+//         req.userId = decoded.id
+//         next()
+//     })
+// }
+
+// export default authMiddleware
