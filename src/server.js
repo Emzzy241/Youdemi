e0429506d915cf3c373c77d12ed2799b57532844
Emzzy241 <emzzyoluwole@gmail.com>
2025-07-05 15:46:17 +0100

    update server.js with new routes

--- src/server.js
+++ src/server.js
@@ -4,7 +4,12 @@ import { fileURLToPath } from 'url';
 import authRoutes from './routes/authRoutes.js'
 import authMiddleware from './middlewares/authMiddleware.js';
 import appRoutes from './routes/appRoutes.js'
-
+import userRoutes from './routes/userRoutes.js'
+import courseRoutes from './routes/courseRoutes.js'
+import kycRoutes from './routes/kycRoutes.js'
+import orderRoutes from './routes/orderRoutes.js'
+import paymentRoutes from './routes/paymentRoutes.js'
+// THank you for Watching, Dynasty :)
 
 const app = express()
 const PORT = process.env.PORT || 5000
@@ -25,6 +30,14 @@ app.get('/dashboard', (req, res) => {
     res.sendFile(path.join(__dirname, '../public', 'dashboard.html'))
 }) // add middleware here to prevent users from gaining access to the dashboard unless they sign up
 
+
+app.use('/api/auth', authRoutes)
+app.use('/api/users', userRoutes)
+app.use('/api/courses', courseRoutes)
+app.use('/api/orders', orderRoutes)
+app.use('/api/payments', paymentRoutes)
+app.use('/api/kyc', kycRoutes)
+// app.s 
 
 app.use('/auth', authRoutes)
 app.use('/app', authMiddleware, appRoutes)
